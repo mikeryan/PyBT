482f324ad03956a4ab4ced02f65a1a3caef18849
Richo Healey <richo@psych0tik.net>
2015-02-04 01:35:48 -0800

    Refactor guts into gatt_core
    This gives you enough primitives to run the gatt_client programmatically
    without a repl

--- PyBT/gattclient.py
+++ PyBT/gattclient.py
@@ -4,11 +4,9 @@ import code
 import argparse
 from threading import Thread
 import gevent
-from gevent.fileobject import FileObject
 from binascii import unhexlify
 from PyBT.roles import LE_Central
-from PyBT.stack import BTEvent
-from PyBT.gap import GAP
+from PyBT.gatt_core import Connection, ConnectionError
 
 from gevent.select import select
 # this is hack because the above does not work
@@ -19,7 +17,6 @@ DISCONNECTED = 0
 CONNECTED = 1
 
 central = None
-seen = {}
 state = DISCONNECTED
 onconnect = []
 
@@ -130,14 +127,6 @@ class CommandModule(object):
             return None
         return ('raw', data)
 
-    @staticmethod
-    def onconnect(*args):
-        subcommand = parse_command(args, False)
-        if subcommand[0] == 'onconnect':
-            raise InvalidCommand("Can't nest onconnects")
-        return ('onconnect', subcommand)
-
-
 COMMANDS = {
     'scan': CommandModule.scan,
     'connect': CommandModule.connect,
@@ -149,7 +138,8 @@ COMMANDS = {
     'onconnect': CommandModule.onconnect,
 }
 
-def parse_command(f, recurse=True):
+
+def parse_command(f):
     if len(f) == 0:
         return None
     cmd_name = f[0]
@@ -157,152 +147,63 @@ def parse_command(f, recurse=True):
         cmd = COMMANDS[cmd_name](*f[1:])
         return cmd
     except IndexError:
-        pass # Ignore people mushing return
+        pass  # Ignore people mushing return
     except KeyError as e:
         print "Error: Unknown command '%s'" % e.args[0]
         raise UnknownCommand("unknown: %s" % e.args[0])
     except InvalidCommand as e:
-        print(repr(e)) # TODO Deal more gracefully
+        print(repr(e))  # TODO Deal more gracefully
 
-def process_command(cmd):
-    global seen, state, onconnect
-
-    if cmd[0] == 'scan':
-        if cmd[1] == 'on':
-            central.stack.scan()
-        else:
-            central.stack.scan_stop()
-    elif cmd[0] == 'connect':
-        addr, type = cmd[1:3]
-        if type is None:
-            type = seen.get(addr, (None,))[0] # maybe we saw it when advertising
-        if type is None:
-            print "Error: please give address type"
-        else:
-            print "Connecting.."
-            central.stack.connect(addr, type)
-    elif cmd[0] == 'quit':
-        exit(0)
-    elif cmd[0] == 'write-req' or cmd[0] == 'write-cmd':
-        if state != CONNECTED:
-            print "Can only write when connected!"
-        else:
-            if cmd[0] == 'write-req':
-                central.att.write_req(handle=cmd[1], value=cmd[2])
-            else:
-                central.att.write_cmd(handle=cmd[1], value=cmd[2])
-    elif cmd[0] == 'read':
-        handle = cmd[1]
-        if state != CONNECTED:
-            print "Can only read when connected!"
-        else:
-            central.att.read(handle)
-    elif cmd[0] == 'interval':
-        central.stack.interval_min = cmd[1]
-        central.stack.interval_max = cmd[2]
-    elif cmd[0] == 'onconnect':
-        onconnect.append(cmd[1])
-    elif cmd[0] == 'eval':
-        try:
-            cmd[1]()
-        except Exception as e:
-            print "Error running eval'd code: %s" % e
-    elif cmd[0] == 'raw':
-        central.stack.raw_att(cmd[1])
-
-def command_handler():
-    while True:
-        sys.stdout.write('>>> ')
-        sys.stdout.flush()
-
-        select([sys.stdin], [], [])
-        line = sys.stdin.readline()
-
-        # handle commands
-        if len(line) == 0:
-            print
-            exit(0) # hack for control-D
-        line = line.strip()
-        cmd = parse_command(line.split())
-        if cmd is not None:
-            process_command(cmd)
-
-def dump_gap(data):
-    if len(data) > 0:
-        try:
-            gap = GAP()
-            gap.decode(data)
-            print "GAP: %s" % gap
-        except Exception as e:
-            print e
-            pass
 
 def socket_handler(central):
     global seen, state, onconnect
 
     # handle events
-    while True:
-        select([central.stack], [], [])
-        event = central.stack.handle_data()
-        if event.type == BTEvent.SCAN_DATA:
-            addr, type, data = event.data
-            print "Saw %s (%s)" % (addr, "public" if type == 0 else "random")
-            if addr in seen:
-                if len(data) > len(seen[addr][1]):
-                    seen[addr] = (type, data)
-                    dump_gap(data)
+
+
+def runsource_with_connection(connection):
+    # orig_runsource = code.InteractiveConsole.runsource
+
+    def runsource(self, source, filename='<input>',
+                  symbol='single', encode=True):
+        # Try parsing it as a gatt client thing, then fall back to python
+        debug("[-] %s" % repr(source.split()))
+        oncommand_hack = False
+        try:
+            parts = source.split()
+            if parts[0] == 'oncommand':
+                oncommand_hack = True
+                parts.pop(0)
+            # FIXME(richo) Find out what version gives short syntax
+            args = parse_command(parts)
+            cmd = args.pop(0).replace('-', '_')
+        except UnknownCommand:
+            # XXX uncomment me to make this into a python repl
+            # return orig_runsource(self, source)
+            return None
+
+        if cmd is not None:
+            debug("[-] %s(%s)" % (repr(cmd), repr(args)))
+            # FIXME(richo) Deal gracefully with the AttributeError
+            func = getattr(connection, cmd)
+            if oncommand_hack:
+                self.connection.oncommand(lambda: func(*args))
             else:
-                seen[addr] = (type, data)
-                dump_gap(data)
+                func(*args)
+    return runsource
 
-        elif event.type == BTEvent.CONNECTED:
-            state = CONNECTED
-            print "Connected!"
-            if len(onconnect) > 0:
-                print "Running onconnect comands"
-                for i in onconnect:
-                    process_command(i, central)
-                onconnect = []
-        elif event.type == BTEvent.DISCONNECTED:
-            state = DISCONNECTED
-            print "Disconnected"
-
-        elif event.type == BTEvent.ATT_DATA:
-            pkt = event.data
-            # ack handle value notification
-            if pkt.opcode == 0x1d:
-                central.stack.raw_att("\x1e")
-            print event
-        elif event.type != BTEvent.NONE:
-            print event
-
-
-orig_runsource = code.InteractiveConsole.runsource
-def runsource(self, source, filename='<input>', symbol='single', encode=True):
-    # Try parsing it as a gatt client thing, then fall back to python
-    debug("[-] %s" % repr(source.split()))
-    try:
-        cmd = parse_command(source.split())
-    except UnknownCommand:
-        # XXX uncomment me to make this into a python repl
-        # return orig_runsource(self, source)
-        return None
-
-    if cmd is not None:
-        debug("[-] %s" % repr(cmd))
-        process_command(cmd)
 
 def main():
-    global central
-
     parser = _argparser()
     args = parser.parse_args()
 
     central = LE_Central(adapter=args.interface)
-    gevent.spawn(socket_handler, central)
 
-    code.InteractiveConsole.runsource = runsource
-    Thread(target=code.interact, kwargs={'local': locals()}).start()
+    connection = Connection(central)
+    connection.start()
+
+    code.InteractiveConsole.runsource = runsource_with_connection(connection)
+    Thread(target=code.interact, local=locals()).start()
 
     gevent.wait()
 
